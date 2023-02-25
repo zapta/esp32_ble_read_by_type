8833f81c8ded64029c89e9a9ea52cef0a44c35e5
Zapta <zapta@users.noreply.github.com>
2023-02-24 18:51:58 -0800

    More cleanup.

--- python/test.py
+++ python/test.py
@@ -14,6 +14,7 @@ print(f"OS: {platform.platform()}", flush=True)
 print(f"Platform:: {platform.uname()}", flush=True)
 print(f"Python {sys.version}", flush=True)
 
+
 async def test():
     print(f"Trying to connect to {device_address}", flush=True)
     device = await BleakScanner.find_device_by_address(device_address, timeout=10.0)
@@ -22,20 +23,19 @@ async def test():
         print(f"Device found.", flush=True)
         assert client.is_connected
         print(f"Connected found.", flush=True)
-        service = client.services.get_service("6b6a78d7-8ee0-4a26-ba7b-62e357dd9720")
-        assert(service)
+        service = client.services.get_service(
+            "6b6a78d7-8ee0-4a26-ba7b-62e357dd9720")
+        assert (service)
         print(f"Service found.", flush=True)
         chrc = service.get_characteristic("ff06")
-        assert(chrc)
+        assert (chrc)
         print(f"Characteristic found.", flush=True)
         for i in range(5):
-          print(f"\n{i + 1} Writing...", flush=True)
-          await client.write_gatt_char(chrc, bytearray([0x06, 0x03]))
-          print("Wirting done.")
-          await asyncio.sleep(0.2)
+            print(f"\n{i + 1} Writing...", flush=True)
+            await client.write_gatt_char(chrc, bytearray([0x06, 0x03]))
+            print("Writing done.")
+            await asyncio.sleep(0.2)
         print(f"\nAll done.", flush=True)
-        print(f"Type Ctrl-C to abort.", flush=True)
-    print(f"111", flush=True)
+        print(f"Ctl-C to exit.", flush=True)
 
 asyncio.run(test())
-print(f"222", flush=True)
